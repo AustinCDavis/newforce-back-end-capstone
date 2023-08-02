8e636ffd9fc8c5b9c83dc39b38b09d57ce2c091d
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:10:30 -0400

    Completed Reaction Model

--- Backend-Capstone/BackEndCapstone/Models/Reaction.cs
+++ Backend-Capstone/BackEndCapstone/Models/Reaction.cs
@@ -1,6 +1,17 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class Reaction
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string IconCode { get; set; }
     }
 }
