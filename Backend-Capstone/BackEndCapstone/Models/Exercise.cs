948b8a94e76361e3c59f83738bd7bd67ff4ff30a
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:09:06 -0400

    Completed Exercise Model

--- Backend-Capstone/BackEndCapstone/Models/Exercise.cs
+++ Backend-Capstone/BackEndCapstone/Models/Exercise.cs
@@ -1,6 +1,30 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class Exercise
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Type { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Muscle { get; set; }
+
+        [Required]
+        [MaxLength(4000)]
+        public string Instructions { get; set; }
+
+        [DataType(DataType.Url)]
+        [MaxLength(255)]
+        public string VideoLocation { get; set; }
+
     }
 }
