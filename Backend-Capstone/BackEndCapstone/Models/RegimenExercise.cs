79bafe44e3723defe95474eee33b4c1454d40c5f
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:16:18 -0400

    Completed RegimenExercise Model

--- Backend-Capstone/BackEndCapstone/Models/RegimenExercise.cs
+++ Backend-Capstone/BackEndCapstone/Models/RegimenExercise.cs
@@ -1,6 +1,17 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class RegimenExercise
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int RegimenId { get; set; }
+        public Regimen? Regimen { get; set; }
+
+        [Required]
+        public int ExerciseId { get; set; }
+        public Exercise? Exercise { get; set; }
     }
 }
