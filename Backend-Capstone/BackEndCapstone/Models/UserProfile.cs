623fce512ee3235960b0ab3bd3b81a10edf9dd75
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 21:55:45 -0400

    Completed UserProfile Model

--- Backend-Capstone/BackEndCapstone/Models/UserProfile.cs
+++ Backend-Capstone/BackEndCapstone/Models/UserProfile.cs
@@ -1,6 +1,45 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class UserProfile
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime CreateDateTime { get; set; }
+
+        [DataType(DataType.Url)]
+        [MaxLength(255)]
+        public string ImageLocation { get; set; }
+
+        [Required]
+        public int UserTypeId { get; set; }
+        public UserType? UserType { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
     }
 }
