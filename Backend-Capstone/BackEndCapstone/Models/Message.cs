1efb1afc7c17a97c37ab0515c4b5b0594040eb1e
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-02 12:03:20 -0400

    Completed note and message models

--- Backend-Capstone/BackEndCapstone/Models/Message.cs
+++ Backend-Capstone/BackEndCapstone/Models/Message.cs
@@ -1,6 +1,24 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class Message
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int ToId { get; set; }
+
+        [Required]
+        public int FromId { get; set; }
+        public UserProfile? ToUserProfile { get; set; }
+        public UserProfile? FromUserProfile { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreateDateTime { get; set; }
     }
 }
--- Backend-Capstone/BackEndCapstone/Models/Note.cs
+++ Backend-Capstone/BackEndCapstone/Models/Note.cs
@@ -1,6 +1,24 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class Note
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int ProviderProfileId { get; set; }
+
+        [Required]
+        public int PatientProfileId { get; set; }
+        public UserProfile? ProviderProfile { get; set; }
+        public UserProfile? PatientProfile { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreateDateTime { get; set; }
     }
 }
