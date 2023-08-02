1a100e15440daf979f3754f3f007929556daab1d
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 21:58:45 -0400

    Completed UserType Model

--- Backend-Capstone/BackEndCapstone/Models/UserType.cs
+++ Backend-Capstone/BackEndCapstone/Models/UserType.cs
@@ -1,6 +1,17 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class UserType
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Type { get; set; }
+
+        public static int ADMIN_ID => 1;
+        public static int PATIENT_ID => 2;
+        public static int PROVIDER_ID => 3;
     }
 }
