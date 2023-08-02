f12e7eb6cf50fb39db768c851a28fd47eb3d3098
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:04:27 -0400

    Completed Comment model

--- Backend-Capstone/BackEndCapstone/Models/Comment.cs
+++ Backend-Capstone/BackEndCapstone/Models/Comment.cs
@@ -1,6 +1,20 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int RegimenExerciseId { get; set; }
+        public RegimenExercise RegimenExercise { get; set; }
+
+        [Required]
+        [MaxLength(4000)]
+        public string Content { get; set; }
+        [Required]
+        public DateTime CreateDateTime { get; set; }
+
     }
 }
