7ff1a809022d452f4ae58990c20d29e603792a72
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:20:34 -0400

    Completed Regimen Model

--- Backend-Capstone/BackEndCapstone/Models/Regimen.cs
+++ Backend-Capstone/BackEndCapstone/Models/Regimen.cs
@@ -1,6 +1,24 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class Regimen
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserProfileId { get; set; }
+        public UserProfile? UserProfile { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(4000)]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime CreateDateTime { get; set; }
     }
 }
