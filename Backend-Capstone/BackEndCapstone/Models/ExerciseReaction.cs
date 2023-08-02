5120ad0041df68a18991d3ac06b3e378fdcd36cb
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:14:21 -0400

    Completed ExerciseReaction Model

--- Backend-Capstone/BackEndCapstone/Models/ExerciseReaction.cs
+++ Backend-Capstone/BackEndCapstone/Models/ExerciseReaction.cs
@@ -1,6 +1,17 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class ExerciseReaction
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int RegimenExerciseId { get; set; }
+        public RegimenExercise? RegimenExercise { get; set; }
+
+        [Required]
+        public int ReactionId { get; set; }
+        public Reaction? Reaction { get; set; }
     }
 }
