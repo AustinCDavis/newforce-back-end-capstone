1835e7fb92e4985d679a5ef435736137812c744e
Austin Davis <davis.austin1.1.c@gmail.com>
2023-08-01 22:24:44 -0400

    Completed RegimenAssignment Model

--- Backend-Capstone/BackEndCapstone/Models/RegimenAssignment.cs
+++ Backend-Capstone/BackEndCapstone/Models/RegimenAssignment.cs
@@ -1,6 +1,20 @@
-﻿namespace BackEndCapstone.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BackEndCapstone.Models
 {
     public class RegimenAssignment
     {
+        public int Id { get; set; }
+
+        [Required]
+        public int RegimenId { get; set; }
+        public Regimen? Regimen { get; set; }
+
+        [Required]
+        public int PatientProfileId { get; set; }
+        public UserProfile? UserProfile { get; set; }
+
+        [Required]
+        public DateOnly AssignmentDate { get; set; }
     }
 }
